09c7f264011ced1f4ee43cffa6216352b6694066
Sahar <77019488+SaharAbdelhaq@users.noreply.github.com>
2023-01-19 11:10:21 +0200

    commit
    node package

---
+++ first-node-app/index.js
@@ -0,0 +1,18 @@
+const listBooks = () => {
+    return [
+      {
+        title: "Designing Data Intensive Applications",
+        author: "Martin Kleppmann",
+      },
+      { title: "The Psychology of Money", author: "Morgan Housel" },
+    ];
+  };
+  
+  console.log(listBooks())
+
+
+  //we try to create an custom package... 
+  //const calculateSquareArea = (x) => x*x;
+//export default calculateSquareArea
+
+  
