5b95da882c31963d42871d3acb5be6bfeeab389c
Sahar <77019488+SaharAbdelhaq@users.noreply.github.com>
2023-01-19 16:55:15 +0200

    committ

---
+++ basic ExpressJS app/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.send('<h1>Welcome to you first Express App</h1>');
+});
+
+app.listen(3000, () =>
+  console.log(`🚀 Server ready at: http://localhost:3000`)
+);
+
